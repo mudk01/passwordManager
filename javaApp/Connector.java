64853f2911360669fb9ae631a926773f3e2e959f
Mueed Khalid <51312849+mudk01@users.noreply.github.com>
2020-12-22 02:00:37 -0800

    Created java file that establishes connection to local database

--- javaApp/Connector.java
+++ javaApp/Connector.java
@@ -1,1 +1,14 @@
+import java.sql.*;
 
+public class Connector {
+    public static void main(String[] args) {
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/passwordDb", "testuser", "securepassword");
+        Statement stmnt = conn.createStatement();
+        try () {
+
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
