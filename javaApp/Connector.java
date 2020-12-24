e2831f8897661495ce4b87d4c07f147c7461c4ed
Mueed Khalid <51312849+mudk01@users.noreply.github.com>
2020-12-23 18:02:30 -0800

    added to package and attempted to add a prompt obj from prompt.java

--- javaApp/Connector.java
+++ javaApp/Connector.java
@@ -1,12 +1,19 @@
+package jdbc;
+import jdbc.prompt;
 import java.sql.*;
 
 public class Connector {
     public static void main(String[] args) {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/passwordDb", "testuser", "securepassword");
-        Statement stmnt = conn.createStatement();
-        try () {
+        prompt pr = new prompt();
+        pr.connectPrompt();
+        // if(pr.connectPrompt() == 0)
+        //     return;
 
-        }
+        try (
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/passwordManager?allowPublicKeyRetrieval=true&useSSl=falserverTimezone=UTC", "testuser", "securepassword");
+            Statement stmnt = conn.createStatement()) {
+
+            }
         catch (SQLException e) {
             e.printStackTrace();
         }
