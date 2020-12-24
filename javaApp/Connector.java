168d41d2a2e987539f523842eaf55b323dbb8a66
Mueed Khalid <51312849+mudk01@users.noreply.github.com>
2020-12-23 19:24:02 -0800

    added select statement to test connection

--- javaApp/Connector.java
+++ javaApp/Connector.java
@@ -5,14 +5,21 @@ import java.sql.*;
 public class Connector {
     public static void main(String[] args) {
         prompt pr = new prompt();
-        pr.connectPrompt();
+        //pr.connectPrompt();
         // if(pr.connectPrompt() == 0)
         //     return;
 
         try (
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/passwordManager?allowPublicKeyRetrieval=true&useSSl=falserverTimezone=UTC", "testuser", "securepassword");
-            Statement stmnt = conn.createStatement()) {
-
+                Statement stmnt = conn.createStatement()) 
+            {
+                String strSelect = "select password from accounts where website like '%facebook%'";
+                ResultSet rSet = stmnt.executeQuery(strSelect);
+                while(rSet.next()) {
+                    String password = rSet.getString("password");
+                    System.out.println(password);
+                }       
+                
             }
         catch (SQLException e) {
             e.printStackTrace();
