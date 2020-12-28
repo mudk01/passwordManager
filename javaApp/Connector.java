49d0f73897b533cafad4392c04cef750cde5cb5f
Mueed Khalid <51312849+mudk01@users.noreply.github.com>
2020-12-27 20:56:02 -0800

    New Methods
    Created an exit prompt, a username method to return user inputted username, and a passPrompt method to prompt user how they would like to get their information

--- javaApp/Connector.java
+++ javaApp/Connector.java
@@ -1,14 +1,10 @@
 package jdbc;
 import jdbc.prompt;
 import java.sql.*;
+import java.util.*;
 
 public class Connector {
     public static void main(String[] args) {
-        prompt pr = new prompt();
-        //pr.connectPrompt();
-        // if(pr.connectPrompt() == 0)
-        //     return;
-
         try (
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/passwordManager?allowPublicKeyRetrieval=true&useSSl=falserverTimezone=UTC", "testuser", "securepassword");
                 Statement stmnt = conn.createStatement()) 
@@ -18,11 +14,58 @@ public class Connector {
                 while(rSet.next()) {
                     String password = rSet.getString("password");
                     System.out.println(password);
-                }       
+                }
+                exitPrompt();    
+                passPrompt();   
                 
             }
         catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    static void exitPrompt() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the password manager.\nTo exit, please 'enter' exit or 'q'.");
+        String userIn = sc.nextLine();
+        if(userIn.equals("q") || userIn.equals("exit"))
+            System.exit(0);
+        else
+            return;
+    }
+
+    static String userName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("You have selected 'Find by username. Please enter the username you would like to use.");
+        String usrNm = sc.nextLine();
+        String sqlSelectUsrNm = "select * from accounts where username = '" + usrNm + "'";
+        return sqlSelectUsrNm;
+    }
+
+    static void passPrompt() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How would you like to find your password?\nYou can view your password for a single site by entering the site name, or enter a username and view all associated accounts.\nPlease enter your choice below.");
+        String ppIn = sc.nextLine();
+        String strSelectUsrNm = userName();
+        if(ppIn.equals("username")){
+            try (
+                Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/passwordManager?allowPublicKeyRetrieval=true&useSSl=falserverTimezone=UTC", "testuser", "securepassword");
+                    Statement stmnt = conn.createStatement()) 
+                {
+                    // String strSelectUsrNm = userName();
+                    ResultSet usrSet = stmnt.executeQuery(strSelectUsrNm);
+                    while(usrSet.next()) {
+                        String usrWebsite = usrSet.getString("website");
+                        String usrEmail = usrSet.getString("email");
+                        String usrUsername = usrSet.getString("username");
+                        String usrPassword = usrSet.getString("password");
+                        System.out.println("Website: " + usrWebsite + " " + "Email: " + usrEmail + " " + "Username: " + usrUsername + " " + "Password: " + usrPassword);
+                    }  
+                    
+                }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
